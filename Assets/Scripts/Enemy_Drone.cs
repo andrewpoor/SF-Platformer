7b23e7cc1949a21a3b286bc0537a4e0a9cd8df37
Andrew Poor <andrew.poor@imperial.ac.uk>
2024-01-11 16:13:23 +0000

    Started work on simple drone enemy. Currently moves back and forth with a turning animation.

---
+++ Assets/Scripts/Enemy_Drone.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using UnityEngine;
+
+public class Enemy_Drone : MonoBehaviour
+{
+    //References.
+    [SerializeField] private Animator animator;
+
+    //Parameters.
+    [SerializeField] private float patrolDistance = 2.0f;
+    [SerializeField] private float moveSpeed = 1.0f;
+    [SerializeField] private bool moveLeft = true;
+
+    private bool turning = false;
+    private bool alive = true;
+
+    void Start()
+    {
+        //Start facing the direction it needs to move in. Default is facing left.
+        if(!moveLeft)
+        {
+            ChangeDirection();
+        }
+
+        StartCoroutine(Patrol());
+    }
+
+    //Movement pattern for this enemy.
+    private IEnumerator Patrol()
+    {
+        while(alive)
+        {
+            animator.SetBool("Turning", false); //Play move animation.
+
+            float distanceTravelled = 0.0f;
+
+            //Move in the facing direction until full distance is reached.
+            while(distanceTravelled < patrolDistance)
+            {
+                float frameMovement = moveSpeed * Time.deltaTime;
+                float remainingDistance = patrolDistance - distanceTravelled;
+
+                if(remainingDistance < frameMovement)
+                {
+                    //Set movement so the drone moves exactly to reach the full distance.
+                    frameMovement = remainingDistance;
+                    distanceTravelled = patrolDistance;
+                }
+                else
+                {
+                    distanceTravelled += frameMovement;
+                }
+
+                transform.position += new Vector3(moveLeft ? -frameMovement : frameMovement, 0.0f, 0.0f);
+
+                yield return null;
+            }
+            
+            //Turn around.
+            animator.SetBool("Turning", true); //Play turning animation.
+            turning = true;
+            moveLeft = !moveLeft;
+
+            //Wait until animation event indicating the drone has finished turning around.
+            while(turning)
+            {
+                yield return null;
+            }
+        }
+    }
+
+    //Change direction drone is facing. Called by an animation event.
+    void ChangeDirection()
+    {
+        Vector3 curScale = transform.localScale;
+        transform.localScale = new Vector3(-curScale.x, curScale.y, curScale.z);
+        turning = false;
+    }
+}
